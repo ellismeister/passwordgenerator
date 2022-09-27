357067cada3836feefced6ffb3b36d7dea789fb9
Meister <meistercodes@gmail.com>
2022-09-27 14:51:50 -0400

    pw

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // @ts-nocheck
 const program = require('commander');
 const chalk = require('chalk');
+const clipboardy = require('clipboardy');
 const log = console.log;
 const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
 
 program.version('1.0.0').description('CLI Password Generator');
 
@@ -18,5 +20,18 @@ const { length, save, numbers, symbols } = program.opts();
 // Get Generated Password
 const generatedPassword = createPassword(length, numbers, symbols);
 
+// Save to clipboard
+if (save) {
+  savePassword(generatedPassword);
+}
+
+// Copy to Clipboard
+clipboardy.writeSync(generatedPassword);
+
 // Output Generated Password
-log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+log(
+  chalk.hex('#0542fa').bold('Generated Password: ') +
+    chalk.hex('#fa2a05').bold(generatedPassword)
+);
+
+log(chalk.yellow('Password copied to clipboard'));
