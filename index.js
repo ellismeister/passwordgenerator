0a45228244f986534242ee0c440329a854d422bd
Meister <meistercodes@gmail.com>
2022-09-27 13:41:02 -0400

    index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// @ts-nocheck
 const program = require('commander');
 
 program.version('1.0.0').description('CLI Password Generator');
 
 program
-  .command('generate')
-  .action(() => {
-    console.log('Test');
-  })
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to passwords.txt')
+  .option('-nn, --no-numbers', 'remove numbers from password')
+  .option('-ns, --no-symbols', 'remove symbols from password')
   .parse();
+
+console.log(program.opts());
